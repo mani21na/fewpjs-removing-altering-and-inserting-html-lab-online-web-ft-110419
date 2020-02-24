5dfe7921aa07423b8976fd6444634388bb9f7c34
Hanna Lee <mani21na@gmail.com>
2020-02-24 23:26:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-let element = document.createElement('div');
-document.body.appendChild(element);
+document.querySelector('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'YOUR-NAME is the champion'
